3937265a53152f491bb8069f96835bd5cfb8bf9c
Paul Hoover <paul.hoover@selectablemedia.com>
2013-12-16 18:19:40 -0500

    device_utils.js added

---
+++ js/device_util.js
@@ -0,0 +1,17 @@
+if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", function () {
+        tilt([event.beta, event.gamma]);
+    }, true);
+} else if (window.DeviceMotionEvent) {
+    window.addEventListener('devicemotion', function () {
+        tilt([event.acceleration.x * 2, event.acceleration.y * 2]);
+    }, true);
+} else {
+    window.addEventListener("MozOrientation", function () {
+        tilt([orientation.x * 50, orientation.y * 50]);
+    }, true);
+}
+
+
+// compass: 
+// http://stackoverflow.com/questions/16317599/android-compass-that-can-compensate-for-tilt-and-pitch
