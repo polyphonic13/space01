e867c16dc96bd91b0357cedadeaeb40d77ca75e2
paul hoover <paulhhoover@hotmail.com>
2013-12-25 17:16:52 -0500

    control_layer: trace statements commented out

--- public/src/js/views/controls/control_layer.js
+++ public/src/js/views/controls/control_layer.js
@@ -56,13 +56,13 @@ var ControlLayer = (function() {
 	}
 	
 	function _onPressed(evt) {
-		trace('ControlLayer/_onPressed');
-		trace(evt);
+		// trace('ControlLayer/_onPressed');
+		// trace(evt);
 		var hits = _checkHitRegions(evt);
-		trace('\tpost check hit regions, hits = ');
-		trace(hits);
+		// trace('\tpost check hit regions, hits = ');
+		// trace(hits);
 		for(var key in hits) {
-			trace('hits['+key+'] = ' + hits[key]);
+			// trace('hits['+key+'] = ' + hits[key]);
 			if(hits[key]) {
 				_pressedButtons[key] = true;
 				// do not allow for both forward and reverse to be true at the same time:
@@ -74,13 +74,13 @@ var ControlLayer = (function() {
 			}
 			// _pressedButtons[key] = hits[key];
 		}
-		trace('POST PRESS, _pressedButtons = ');
-		trace(_pressedButtons);
+		// trace('POST PRESS, _pressedButtons = ');
+		// trace(_pressedButtons);
 	}
 	
 	function _onReleased(evt) {
-		trace('ControlLayer/_onReleased');
-		trace(evt);
+		// trace('ControlLayer/_onReleased');
+		// trace(evt);
 		var hits = _checkHitRegions(evt);
 		var hit;
 		var noButtons = true;
@@ -102,8 +102,8 @@ var ControlLayer = (function() {
 				_pressedButtons[key] = false;
 			}
 		}
-		trace('POST RELEASE, _pressedButtons = ');
-		trace(_pressedButtons);
+		// trace('POST RELEASE, _pressedButtons = ');
+		// trace(_pressedButtons);
 	}
 	
 	function _checkHitRegions(evt) {
@@ -128,14 +128,14 @@ var ControlLayer = (function() {
 		var xHit = (pos.x > hitRegion.x && pos.x < hitRegion.width) ? true : false;
 		var yHit = (pos.y > hitRegion.y && pos.y < hitRegion.height) ? true : false;
 		var hit = (xHit && yHit) ? true : false;
-		trace('_checkHitRegion\n\t\tpos: ' + pos.x + '/' + pos.y + '\n\t\thitRegion: ' + hitRegion.x + '/' + hitRegion.y + ', ' + hitRegion.width + '/' + hitRegion.height + '\n\t\txHit = ' + xHit + ', yHit = ' + yHit + ', hit = ' + hit);
+		// trace('_checkHitRegion\n\t\tpos: ' + pos.x + '/' + pos.y + '\n\t\thitRegion: ' + hitRegion.x + '/' + hitRegion.y + ', ' + hitRegion.width + '/' + hitRegion.height + '\n\t\txHit = ' + xHit + ', yHit = ' + yHit + ', hit = ' + hit);
 		return hit;
 	}
 	
 	function _getEventPosition(evt) {
 		var pos = {};
 		if(evt.changedTouches && evt.changedTouches.length > 0) {
-			// trace('there are changedTouches');
+			// // trace('there are changedTouches');
 			pos = {
 				// x: evt.targetTouches[0].clientX,
 				// y: evt.targetTouches[0].clientY
@@ -143,7 +143,7 @@ var ControlLayer = (function() {
 				y: evt.changedTouches[0].clientY
 			}
 		} else {
-			// trace('no target touches');
+			// // trace('no target touches');
 			pos = {
 				x: evt.x,
 				y: evt.y
