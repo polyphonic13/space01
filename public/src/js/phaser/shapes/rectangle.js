69630d3f9db5247a95710e736d96438d7f5e71f9
paul hoover <paulhhoover@hotmail.com>
2014-01-10 00:04:05 -0500

    work on shapes and menu state

---
+++ public/src/js/phaser/shapes/rectangle.js
@@ -0,0 +1,14 @@
+Polyworks.Rectangle = (function() {
+	
+	function Rectangle(params) {
+		trace('Rectangle, params = ');
+		trace(params);
+		var graphics = params.game.add.graphics(params.graphics.x, params.graphics.y);
+		graphics.lineStyle(params.stroke.width, params.stroke.color, params.stroke.opacity);
+		graphics.beginFill(params.fill.color, params.fill.alpha);
+		graphics.drawRect(params.x, params.y, params.width, params.height);
+		graphics.endFill();
+	}
+	
+	return Rectangle;
+})();
