8c4d2c1b2bf7c68154f56baf7f3e8a05b68fa2a3
paul hoover <paulhhoover@hotmail.com>
2014-03-03 21:41:17 -0500

    more map state work (still not building)

--- public/src/js/phaser/data/keke2_config7.js
+++ public/src/js/phaser/data/keke2_config7.js
@@ -8619,6 +8619,67 @@ Polyworks.Config = (function() {
 					]
 				},
 				{
+					name: 'mapPages',
+					cl: 'GroupCollection',
+					attrs: [
+					{
+						name: 'page1',
+						cl: 'MapPage',
+						levels: [
+							'level1',
+							'level2',
+							'level3',
+							'level4'
+						],
+						attrs: [
+						{
+							name: 'subtitle',
+							cl: 'Text',
+							attrs: {
+								centerX: true,
+								centerY: false,
+								x: 0,
+								y: (stageUnit * 1.5),
+								defaultContent: 'Chapter 1: The Foggy Forest',
+								style: {
+									font: '22px Arial',
+									fill: '#ffffff',
+									align: 'center'
+								}
+							}
+						}
+						]
+					},
+					{
+						name: 'page2',
+						cl: 'MapPage',
+						levels: [
+							'level5',
+							'level6'
+						],
+						attrs: [
+						{
+							name: 'subtitle',
+							cl: 'Text',
+							attrs: {
+								centerX: true,
+								centerY: false,
+								x: 0,
+								y: (stageUnit * 1.5),
+								defaultContent: 'Chapter 1: The Misty Mountain',
+								style: {
+									font: '22px Arial',
+									fill: '#ffffff',
+									align: 'center'
+								}
+							}
+
+						}
+						]
+					}
+					]
+				},
+				{
 					name: 'mapControls',
 					cl: 'ControlButtons',
 					type: 'map',
--- public/src/js/phaser/states/map_state.js
+++ public/src/js/phaser/states/map_state.js
@@ -7,21 +7,25 @@ Polyworks.MapState = (function() {
 
 	MapState.prototype.createState = function() {
 		trace('MapState/createState, this = ', this);
-		MapState._super.createState.call(this);
-		var pages = this.model.pages;
-		trace('\tpages = ', pages);
-		this.model.pageCollection = [];
-		var mapPage;
+		// MapState._super.createState.call(this);
+		// var pages = this.model.pages;
+		// trace('\tpages = ', pages);
+		// this.model.pageCollection = [];
+		// var mapPage;
+		// 
+		// Polyworks.Utils.each(pages,
+		// 	function(page) {
+		// 		trace('\tcreating MapPage with: ', page);
+		// 		// mapPage = new Polyworks.MapPage(page);
+		// 		// mapPage.begin();
+		// 		// this.model.pageCollection.push(mapPage);
+		// 		this.model.attrs.push(page);
+		// 	},
+		// 	this
+		// );
 		
-		Polyworks.Utils.each(pages,
-			function(page) {
-				trace('\tcreating MapPage with: ', page);
-				mapPage = new Polyworks.MapPage(page);
-				mapPage.begin();
-				this.model.pageCollection.push(mapPage);
-			},
-			this
-		);
+		MapState._super.createState.call(this);
+		
 	};
 	
 	return MapState;
