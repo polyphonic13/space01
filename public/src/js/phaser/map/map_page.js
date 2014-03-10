83ac5054736f3eb48c8acfb43a2c66aa349bc15e
paul hoover <paulhhoover@hotmail.com>
2014-03-09 20:09:10 -0400

    map changes

--- public/src/js/phaser/map/map_page.js
+++ public/src/js/phaser/map/map_page.js
@@ -3,11 +3,11 @@ Polyworks.MapPage = (function() {
 	
 	function MapPage(params) {
 		this.model = new Polyworks.Model(params);
-		// trace('MapPage['+this.model.name+']/constructor');
+		// // trace('MapPage['+this.model.name+']/constructor');
 	}
 	
 	MapPage.prototype.begin = function() {
-		// trace('MapPage['+this.model.name+']/begin, model = ', this.model);
+		// // trace('MapPage['+this.model.name+']/begin, model = ', this.model);
 		this.addListeners();
 
 		var pageStartX = this.model.start.x;
@@ -30,17 +30,17 @@ Polyworks.MapPage = (function() {
 		
 		if(this.model.leftArrow) {
 			var lBtn = this.addArrowButton('left', pageStartX, stageSeventh);
-			// trace('\t\tlBtn', lBtn);
+			// // trace('\t\tlBtn', lBtn);
 			this.model.attrs.push(lBtn);
 		}
 		if(this.model.rightArrow) {
 			var rBtn = this.addArrowButton('right', pageStartX, stageSeventh);
-			// trace('\t\trBtn', rBtn);
+			// // trace('\t\trBtn', rBtn);
 			this.model.attrs.push(rBtn);
 		}
 
 		MapPage._super.begin.call(this);
-		// trace(this);
+		// // trace(this);
 
 		var _this = this;
 		Polyworks.Utils.each(this.model.collection,
@@ -56,7 +56,7 @@ Polyworks.MapPage = (function() {
 	};
 	
 	MapPage.prototype.onChangeMapPage = function(event) {
-		// trace('MapPage['+this.model.name+']/onChangeMapPage, event = ', event);
+		// // trace('MapPage['+this.model.name+']/onChangeMapPage, event = ', event);
 		if(event.value === this.model.idx) {
 			this.pageGroup.visible = true;
 		} else {
@@ -68,10 +68,11 @@ Polyworks.MapPage = (function() {
 		var levels = this.model.levels;
 		var levelsLength = levels.length;
 		var levelIconWidth = (Polyworks.Stage.unit * 1.5);
+		var levelIconXOffset = (stageSeventh/2) - (levelIconWidth/2);
 		var levelIconHeight = (Polyworks.Stage.unit * 2.25);
 		var stageUnit = Polyworks.Stage.unit;
 		var iconWidthUnit = (stageSeventh/2) - (levelIconWidth/2);
-		trace('============= iconWidthUnit = ' + iconWidthUnit + '\n\tstageSeventh = ' + stageSeventh + '\n\tlevelIconWidth = ' + levelIconWidth);
+		// trace('============= iconWidthUnit = ' + iconWidthUnit + '\n\tstageSeventh = ' + stageSeventh + '\n\tlevelIconWidth = ' + levelIconWidth);
 		// var attrs = this.model.attrs;
 		var attributes = [];
 		var currentLevel = 'level' + (PolyworksGame.currentLevel);
@@ -94,12 +95,12 @@ Polyworks.MapPage = (function() {
 					locked = false;
 				}
 
-				// // trace('\t\tidx = ' + idx);
+				// // // trace('\t\tidx = ' + idx);
 				var start = {};
 				// start.x = (((stageUnit * 3.5) * idx) + (stageUnit * 2)) + pageStartX;
 				// start.x = (iconWidthUnit * idx) + pageStartX;
-				start.x = (stageSeventh * idx) + pageStartX + stageSeventh;
-				trace('\t\t\tstart.x = ' + start.x);
+				start.x = (stageSeventh * idx) + pageStartX + stageSeventh + levelIconXOffset;
+				// trace('\t\t\tstart.x = ' + start.x);
 				start.y = (stageUnit * 4);
 
 				var tempA = _this.model.attrs;
@@ -117,7 +118,7 @@ Polyworks.MapPage = (function() {
 					}
 				);
 				_this.model.attrs = tempA.concat(tempB);
-				trace('\t\t_this.model.attrs = ', _this.model.attrs);
+				// trace('\t\t_this.model.attrs = ', _this.model.attrs);
 			},
 			this
 		);
@@ -126,7 +127,7 @@ Polyworks.MapPage = (function() {
 	};
 	
 	MapPage.prototype.addLevelIcon = function(params) {
-		trace('MapPage['+this.model.name+']/addLevelIcon, params = ', params);
+		// trace('MapPage['+this.model.name+']/addLevelIcon, params = ', params);
 
 		var levelIcon = [
 		{
@@ -215,23 +216,24 @@ Polyworks.MapPage = (function() {
 	};
 	
 	MapPage.prototype.addArrowButton = function(direction, pageStartX, stageSeventh) {
-		trace('MapPage['+this.model.name+']/addArrow, direction = ' + direction + ', start x = ' + pageStartX);
+		// trace('MapPage['+this.model.name+']/addArrow, direction = ' + direction + ', start x = ' + pageStartX);
 		var stageUnit = Polyworks.Stage.unit;
 		var img;
 		var eventValue;
 		var buttonX; 
 		var levelIconWidth = (Polyworks.Stage.unit * 1.5);
+		var levelIconXOffset = (stageSeventh/2) - (levelIconWidth/2);
 		var levelIconHeight = (Polyworks.Stage.unit * 2.25);
 
 		if(direction === 'left') {
 			img = 'pageLeftArrow';
 			eventValue = ((this.model.idx) - 1);
-			buttonX = 0 + pageStartX;
+			buttonX = 0 + pageStartX + levelIconXOffset;
 		} else {
 			img = 'pageRightArrow';
 			eventValue = ((this.model.idx) + 1);
 			// buttonX = (((stageUnit * 3.5) * 4) + (stageUnit * 2)) + pageStartX;
-			buttonX = (stageSeventh * 6) + pageStartX;
+			buttonX = (stageSeventh * 6) + pageStartX + levelIconXOffset;
 		}
 
 		var arrowButton = {
