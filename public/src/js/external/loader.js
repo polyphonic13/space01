14617e1fdfc60914386abbfcc3ee5ce107185017
Paul Hoover <paul.hoover@selectablemedia.com>
2014-04-14 09:19:51 -0400

    moving social stuff from index.html to new social scripts: social_manager.js, facebook.js, gplus.js and twitter.js; external/loader class added

---
+++ public/src/js/external/loader.js
@@ -0,0 +1,62 @@
+Polyworks.Loader = (function() {
+	
+	function Loader() {
+		this.numToLoad = 0;
+		this.numLoaded = 0;
+		this.fileCallback;
+		this.batchCallback;
+		this.scope;
+		this.head = document.getElementsByTagName('head')[0];
+	}
+	
+	Loader.prototype.batchJS = function(files, fileCallback, batchCallback, context) {
+		this.fileCallback = fileCallback || null;
+		this.batchCallback = batchCallback || null;
+		this.context = context || window;
+
+		for(var key in files) {
+			this.loadJS(files[key], key, this.fileLoaded, this);
+		}
+	};
+	
+	Loader.prototype.loadJS = function(url, key, callback, context) {
+		this.numToLoad++;
+
+		var label = key || url;
+
+		var script = document.createElement('script');
+		script.setAttribute('type', 'text/javascript');
+		if (script.readyState) { 
+			script.onreadystatechange = function() {
+				if(script.readyState == 'loaded' || script.readyState == 'complete') {
+					if(callback instanceof Function) {
+						callback.call(context || window, url, true, label);
+					}
+				}
+			};
+		} else {
+			script.onload = function() {
+				if(callback instanceof Function) {
+					callback.call(context || window, url, true, label);
+				}
+			};
+		}
+		script.setAttribute('src', url);
+		this.head.appendChild(script);
+	};
+	
+	Loader.prototype.fileLoaded = function(url, success, key) {
+		if(this.fileCallback !== null) {
+			this.fileCallback.call(this.context, url, success, key);
+		}
+
+		this.numLoaded++;
+		if(this.numLoaded >= this.numToLoad) {
+			if(this.batchCallback !== null) {
+				this.batchCallback.call(this.context);
+			}
+		}
+	};
+	
+	return Loader;
+})();
---
+++ public/src/js/social/social_manager.js
@@ -0,0 +1,16 @@
+Polyworks.SocialManager = (function() {
+
+	var _urls = {
+		facebook: 'facebook.js',
+		gplus: 'gplus.js',
+		twittwer: 'twitter.js'
+	};
+	
+	var socialManager = {
+		init: function(params) {
+
+		}
+	};
+	
+	return socialManager;
+}());
