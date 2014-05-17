35ee31aad884b93f2a5997d02b079b00694518d7
paul hoover <paulhhoover@hotmail.com>
2014-05-17 18:46:10 -0400

    deploy -> build dir

--- grunt/tasks/create_project_html.js
+++ grunt/tasks/create_project_html.js
@@ -4,12 +4,12 @@ module.exports = function(grunt) {
 		function() {
 			console.log('create project html');
 			var project = grunt.config.get('project');
-			var deployDir = grunt.config.get('deployDir');
+			var buildDir = grunt.config.get('buildDir');
 			var preJsHtml = grunt.config.get('preJsHtml');
 			var postJsHtml = grunt.config.get('postJsHtml');
 			var javascriptTag = '\n<script src=\"js/'+project+'.min.js\" type=\"text/javascript\"></script>\n';
 			var htmlSource = preJsHtml + javascriptTag + postJsHtml;
-			var htmlPath = deployDir + '/' + 'index.html';
+			var htmlPath = buildDir + '/' + 'index.html';
 			grunt.file.write(htmlPath, htmlSource);
 		}
 	);
